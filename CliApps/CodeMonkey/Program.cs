26c9982991db46a4fb75d25ebdd14f366357a0ca
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-08-08 21:58:09 -0400

    Code Monkey stuff

---
+++ CliApps/CodeMonkey/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeMonkey{
+    internal class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello, World!");
+            if (1==1) {
+                Console.WriteLine("1 is equal to 1");
+            }
+        }
+    }
+}
